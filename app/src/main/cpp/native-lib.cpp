163fae4cf31ef9af13d150c8b997b5480d6efb5d
Schitrus <schitrus@gmail.com>
2020-02-11 16:32:35 +0100

    Fixed AI?

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,8 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_datx02_120_121_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++ and Kalle";
+    std::string hello;
+    for(int i = 0; i< 10; i++)
+        hello += "Hello from C++ and Kalle again! ";
     return env->NewStringUTF(hello.c_str());
 }
