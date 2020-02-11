57f7b575e714ac478be17cc99af5dc3b1bb60179
danielolssonIT <31478577+danielolssonIT@users.noreply.github.com>
2020-02-11 15:12:55 +0100

    Hello world

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_datx02_120_121_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
