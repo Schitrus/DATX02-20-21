6db8264a3cf8f6744eb4fba2019374e9685df90d
Axel Svensson <axesven@student.chalmers.se>
2020-04-24 13:04:33 +0200

    added the eigen library

---
+++ app/src/main/cpp/fire/util/eigen.h
@@ -0,0 +1,20 @@
+//
+// Created by Shoulder on 2020-04-24.
+//
+
+#ifndef DATX02_20_21_EIGEN_H
+#define DATX02_20_21_EIGEN_H
+
+#include "Eigen/Dense"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace glm;
+
+//float eigen;
+
+void multiply2Matrices();
+
+float calcEigen(vec3 dir);
+
+#endif //DATX02_20_21_EIGEN_H
