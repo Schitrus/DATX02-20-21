723849ff5a60e593c5ad51d9d6ac8f33fb668213
kirderf1 <kirderf4@gmail.com>
2020-04-30 10:53:10 +0200

    Queue change to ConcurrentLinkedQueue

--- app/src/main/java/com/pbf/FireView.java
+++ app/src/main/java/com/pbf/FireView.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 
 import java.util.Queue;
-import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class FireView extends GLSurfaceView {
 
@@ -21,7 +21,7 @@ public class FireView extends GLSurfaceView {
         setEGLConfigChooser(8, 8, 8, 0, 16, 0);
         setEGLContextClientVersion(3);
 
-        Queue<Runnable> inputTaskQueue = new SynchronousQueue<>();
+        Queue<Runnable> inputTaskQueue = new ConcurrentLinkedQueue<>();
 
         renderer = new FireRenderer(inputTaskQueue, context);
         setRenderer(renderer);
